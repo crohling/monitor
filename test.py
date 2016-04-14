de9535e92c560b80e8195428de2c7463e4a889ca
Christian Rohling <crohling88@gmail.com>
2016-04-14 17:47:18 +0000

    Adding travis-ci yaml file

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class BatmanTest(unittest.TestCase):
+
+    def test_batman_principle(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
